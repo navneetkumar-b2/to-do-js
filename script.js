0eb639a6d942ade97a371ccad74c5ad4120b69b6
Navneet Kumar <navneetkumar.html@gmail.com>
2024-03-08 07:35:12 +0530

    now adding array of objects in localStorage

--- script.js
+++ script.js
@@ -3,18 +3,42 @@ let ttl=document.querySelector('.title')
 let desc=document.querySelector('.desc')
 let crossSymbol=document.querySelector('crossSymbol')
 
-addNote=()=>{
-    if(!ttl.value){
-        alert("empty title not allowed")
-    }
-    else{
-        localStorage.setItem(ttl.value, desc.value);
-        
+addNote = () => {
+    if (!ttl.value) {
+        alert("Empty title not allowed");
+    } else {
+        var obj = {
+            title: ttl.value,
+            desc: desc.value,
+            id: Date.now()
+        };
 
-       
-        clearField()  //this was causing problem when 
+        // Retrieve existing todos from localStorage
+        var existingTodos = JSON.parse(localStorage.getItem("todos")) || [];
+
+        // Add the new object to the existing todos
+        existingTodos.push(obj);
+
+        // Update the todos in localStorage
+        localStorage.setItem("todos", JSON.stringify(existingTodos));
+
+        // Perform any additional operations like updating the UI
+        addItem();
+        clearField();
+        console.log(existingTodos);
     }
-    
+};
+
+addItem=()=>{
+    const notesContainer = document.getElementById('notesContainer');
+    const noteElement = document.createElement('div');
+    noteElement.className="notesBox"
+    noteElement.innerHTML = `
+     <div class="crossSymbol">&#x2716;</div>
+     <p class="displayTitle" >${ttl.value}</p>
+     <p class="displayDesc">${desc.value}</p>
+    `;
+    notesContainer.prepend(noteElement);
 }
 clearField=()=>{
     let ttl=document.querySelector('.title')
@@ -24,37 +48,31 @@ clearField=()=>{
 }
 displayNotes=()=>{
     const notesContainer = document.getElementById('notesContainer');
+    const value = localStorage.getItem("todos"); 
+   if(value && value.length > 0) {
+    let to=JSON.parse(value); //this shouls be a array
+
     // Iterate through local storage keys and display the notes
-    for (let i = 0; i < localStorage.length; i++) {
+    for (let i = 0; i < to.length; i++) {
         // <div class="crossSymbol">&#x2716;</div>
         // <div class="checkboxSymbol">&#x2714;</div>
 
-        const key = localStorage.key(i);
-        const value = localStorage.getItem(key);
+        // const value = localStorage.getItem(key);
 
         const noteElement = document.createElement('div');
         noteElement.className="notesBox"
         noteElement.innerHTML = `
          <div class="crossSymbol">&#x2716;</div>
-            <p class="displayTitle" >${key}</p>
-            <p class="displayDesc">${value}</p>
+            <p class="displayTitle" >${to[i].title}</p>
+            <p class="displayDesc">${to[i].desc}</p>
         `;
         notesContainer.appendChild(noteElement);
     }
 }
+}
 
 displayNotes()
-addItem=()=>{
-    const notesContainer = document.getElementById('notesContainer');
-    const noteElement = document.createElement('div');
-    noteElement.className="notesBox"
-    noteElement.innerHTML = `
-     <div class="crossSymbol">&#x2716;</div>
-        <p class="displayTitle" >${ttl.value}</p>
-        <p class="displayDesc">${desc.value}</p>
-    `;
-    notesContainer.prepend(noteElement);
-}
+
 deleteAllNotes=()=>{
     // alert("all notes will be deleted")
     localStorage.clear(); //delete krne se delete to ho gaya,dom ko bhi update krna pdega  

e1adf51bb25716777eae3958db4c94634575aadf
Navneet Kumar <navneetkumar.html@gmail.com>
2024-03-08 07:45:42 +0530

    add functionality now completed , bug fixed

--- script.js
+++ script.js
@@ -23,18 +23,18 @@ addNote = () => {
         localStorage.setItem("todos", JSON.stringify(existingTodos));
 
         // Perform any additional operations like updating the UI
-        addItem();
+        addItem(obj);
         clearField();
-        console.log(existingTodos);
+        
     }
 };
 
-addItem=()=>{
+addItem=(obj)=>{
     const notesContainer = document.getElementById('notesContainer');
     const noteElement = document.createElement('div');
     noteElement.className="notesBox"
     noteElement.innerHTML = `
-     <div class="crossSymbol">&#x2716;</div>
+     <div class="crossSymbol" onclick="deleteToDo(${obj.id})">&#x2716;</div>
      <p class="displayTitle" >${ttl.value}</p>
      <p class="displayDesc">${desc.value}</p>
     `;
@@ -46,33 +46,31 @@ clearField=()=>{
     ttl.value="";
     desc.value="";
 }
-displayNotes=()=>{
+displayNotes = () => {
     const notesContainer = document.getElementById('notesContainer');
-    const value = localStorage.getItem("todos"); 
-   if(value && value.length > 0) {
-    let to=JSON.parse(value); //this shouls be a array
+    const value = localStorage.getItem("todos");
 
-    // Iterate through local storage keys and display the notes
-    for (let i = 0; i < to.length; i++) {
-        // <div class="crossSymbol">&#x2716;</div>
-        // <div class="checkboxSymbol">&#x2714;</div>
+    if (value && value.length > 0) {
+        let todos = JSON.parse(value);
 
-        // const value = localStorage.getItem(key);
-
-        const noteElement = document.createElement('div');
-        noteElement.className="notesBox"
-        noteElement.innerHTML = `
-         <div class="crossSymbol">&#x2716;</div>
-            <p class="displayTitle" >${to[i].title}</p>
-            <p class="displayDesc">${to[i].desc}</p>
-        `;
-        notesContainer.appendChild(noteElement);
+        for (let i = 0; i < todos.length; i++) {
+            const noteElement = document.createElement('div');
+            noteElement.className = "notesBox";
+            noteElement.innerHTML = `
+                <div class="crossSymbol" onclick="deleteToDo(${todos[i].id})">&#x2716;</div>
+                <p class="displayTitle">${todos[i].title}</p>
+                <p class="displayDesc">${todos[i].desc}</p>
+            `;
+            notesContainer.appendChild(noteElement);
+        }
     }
-}
-}
+};
+
 
 displayNotes()
-
+deleteToDo=(id)=>{
+    alert(id)
+}
 deleteAllNotes=()=>{
     // alert("all notes will be deleted")
     localStorage.clear(); //delete krne se delete to ho gaya,dom ko bhi update krna pdega  
